7345a0a6fabec1221952ecd645cd5054d9e15495
therealdope <skheni02@gmail.com>
2025-07-15 23:10:10 +0530

    .

--- src/app/metadata.js
+++ src/app/metadata.js
@@ -4,10 +4,6 @@ export const metadata = {
     openGraph: {
         title: "Ecom | Modern E-commerce Platform",
         description: "Ecom is a powerful e-commerce website built with Next.js, Prisma, and PostgreSQL — optimized for speed, security, and scalability.",
-        url: "https://ecom-skheni.vercel.app",
-        siteName: "Ecom",
         images: ['/logo-white.png'],
-        locale: "en_US",
-        type: "website",
     },
 };
